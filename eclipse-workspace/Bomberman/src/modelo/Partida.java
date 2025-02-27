e7bae892ede168255bf10798d47396ffa4492609
Eneko <enekosumano@gmail.com>
2025-02-27 11:36:55 +0100

    Implementación de Tablero y Partida, estructura base y metodos iniciales

---
+++ eclipse-workspace/Bomberman/src/modelo/Partida.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public class Partida {
+	private Tablero tablero;
+    private int nivel;
+    private boolean juegoActivo;
+
+    public Partida(int filas, int columnas) {
+    	this.tablero= new Tablero(filas, columnas);
+    	this.nivel=1;
+    	this.juegoActivo=true;
+    }
+    
+    public void iniciarJuego() {
+    	System.out.println("¡El juego ha comenzado!");
+    }
+    
+    public void actualizarJuego() {
+    	System.out.println("¡Actualizando el juego!");
+    }
+    
+    public void terminarJuego() {
+        this.juegoActivo = false;
+        System.out.println("El juego ha terminado.");
+    }
+    
+    public boolean estaActivo() {
+        return juegoActivo;
+    }
+    
+}
