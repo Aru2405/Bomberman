4b32d867bf1bce931d7f36f6e01f6445d92452fa
marioech08 <147819584+marioech08@users.noreply.github.com>
2025-03-07 15:46:33 +0100

    Update casilla.java

--- Modelo/casilla.java
+++ Modelo/casilla.java
@@ -1,5 +1,73 @@
 package Modelo;
 
-public class casilla {
+public class Casilla{
+	
+    private Bomba bomba;
+    private BloqueBlando bloqueBlando;
+    private BloqueDuro bloqueDuro;
+
+    public Casilla() {
+        this.bomba = null;
+        this.bloqueBlando = null;
+        this.bloqueDuro = null;
+    }
+
+    public void colocarBomba(Bomba b){
+        this.bomba = b;
+        
+    }
+
+    public boolean tieneBomba(){
+        return bomba != null;
+    }
+
+    public void detonarBomba(){
+        if (bomba != null) {
+        	
+            System.out.println("Bomba exploto en esta casilla");
+            this.bomba = null;
+
+        }
+    }
+
+
+
+    public void colocarBloqueBlando(){
+        this.bloqueBlando = new BloqueBlando();
+        
+    }
+
+    public boolean tieneBloqueBlando(){
+    	
+        return bloqueBlando != null;
+        
+    }
+
+    public void destruirBloqueBlando() {
+        if (tieneBloqueBlando()){
+            bloqueBlando = null;
+            System.out.println("Bloque blando destruido");
+        }
+    }
     
+    public void colocarBloqueDuro(){
+        this.bloqueDuro = new BloqueDuro();
+    }
+
+    public boolean tieneBloqueDuro(){
+        return bloqueDuro != null;
+    }
+
+
+    @Override
+    public String toString(){
+        if (bomba != null){
+            return "[b]";
+        } else if (bloqueBlando != null) {
+            return "[#]";
+        } else if (bloqueDuro != null) {
+            return "[X]";
+        }
+        return "[ ]";
+    }
 }
