25f14ade171d536a634ba8ef958c139ef6d6a476
marioech08 <147819584+marioech08@users.noreply.github.com>
2025-03-07 15:44:57 +0100

    Update bomba.java

--- Modelo/bomba.java
+++ Modelo/bomba.java
@@ -3,61 +3,41 @@ package Modelo;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Bomba{
+public class Bomba{	
 	
     private int x;
     private int y;
-    private int radio=1;
+    private Timer timer; 
 
     public Bomba(int x, int y){
+    	
         this.x = x;
         this.y = y; 
+        this.timer = new Timer();
        
-    }
-    
-    public void iniciarTemporizador() {
-        System.out.println("Temporizador de Bomba en: (" + x + ", " + y + ") iniciado");
+    }    
 
-        new Timer().schedule( 
-            new TimerTask() {
-                @Override
-                public void run() {
-                    Tablero.getTablero().procesarExplosión(obtenerPosicionesAfectadas());
-                }
-            }, 
-            3000//Explota despues de 3 segundos
-        );
-    }
-
-    private int[][] obtenerPosicionesAfectadas() {
-        //Direcciones de la explosion derecha, izquierda, abajo, arriba
-        int[][] direcciones = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};       
-        //posiciones afectadas por la bomba
-        int[][] posicionesAfectadas = new int[direcciones.length + 1][2];
-        //posición de la bomba
-        posicionesAfectadas[0] = new int[]{x, y};
-        // Verificar y agregar posiciones válidas
-        int index = 1;
-        for (int[] direccion : direcciones) {
-            int nuevaX = x + direccion[0];
-            int nuevaY = y + direccion[1];
-            if (Tablero.getTablero().esValida(nuevaX, nuevaY)) {
-                posicionesAfectadas[index++] = new int[]{nuevaX, nuevaY};
+    public void iniciarTemporizador(){
+    	
+        System.out.println("Temporizador iniciado en (" + x + ", " + y + ")");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Bomba exploto en (" + x + ", " + y + ")");
+                Tablero.getTablero().manejarExplosion(x, y);
+                timer.cancel();
             }
-        }
-
-        return posicionesAfectadas;
+        }, 3000);//Explosion después de 3 segundos
     }
 
+    public int getX() {
+        return x;
+    }
 
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-
-
-
+    public int getY() {
+        return y;
+    }
 }
+
+
+
