88963f370aeaed2d973351e0239dc879ca26280e
marioech08 <147819584+marioech08@users.noreply.github.com>
2025-03-07 15:43:12 +0100

    Update Partida.java

--- Modelo/Partida.java
+++ Modelo/Partida.java
@@ -1,30 +1,43 @@
 package Modelo;
 
 public class Partida {
+	
 	private Tablero tablero;
     private int nivel;
     private boolean juegoActivo;
+    private static Partida miPartida;
 
-    public Partida(int filas, int columnas) {
+    private Partida(int filas, int columnas){
     	this.tablero= tablero.getTablero();
     	this.nivel=1;
     	this.juegoActivo=true;
+    	
     }
     
-    public void iniciarJuego() {
-    	System.out.println("¡El juego ha comenzado!");
+    public static Partida getPartida(){
+        if (miPartida == null) {
+        	miPartida = new Partida(11, 17);
+        }
+        return miPartida;
+    }
+
+   
+    
+    public void iniciarJuego(){
+    	System.out.println("El juego ha empezado");
+  
     }
     
-    public void actualizarJuego() {
-    	System.out.println("¡Actualizando el juego!");
+    public void actualizarJuego(){
+    	System.out.println("Actualizando el juego");
     }
     
-    public void terminarJuego() {
+    public void terminarJuego(){
         this.juegoActivo = false;
         System.out.println("El juego ha terminado.");
     }
     
-    public boolean estaActivo() {
+    public boolean estaActivo(){
         return juegoActivo;
     }
     
