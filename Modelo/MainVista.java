ab09f5f0030441bb73eef6404c6ac9f2df49c0d3
Aru2405 <168478320+Aru2405@users.noreply.github.com>
2025-03-25 09:42:11 +0100

    Update MainVista.java

--- Modelo/MainVista.java
+++ Modelo/MainVista.java
@@ -5,8 +5,11 @@ import VistaControlador.VistaJuego;
 public class MainVista {
     public static void main(String[] args) {
         // Ejecutar la interfaz en el hilo de eventos de Swing
+    	
         javax.swing.SwingUtilities.invokeLater(() -> {
-            new VistaJuego(11, 17);
+        	new VistaJuego(11, 17);
+            Tablero.getTablero().notificarCambio();
+
         });
     }
 }
