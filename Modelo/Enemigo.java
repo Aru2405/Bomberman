d02d21cf54a69eb32a5c6757ce061ceba8481786
marioech08 <147819584+marioech08@users.noreply.github.com>
2025-04-09 16:11:47 +0200

    Update Enemigo.java

--- Modelo/Enemigo.java
+++ Modelo/Enemigo.java
@@ -3,28 +3,54 @@ package Modelo;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 public class Enemigo {
     private EstrategiaMovimiento estrategiaMovimiento;
     private int x, y;
     private Timer timer;
 
-    public Enemigo(int x, int y, EstrategiaMovimiento estrategia) {
+    public Enemigo(int x, int y) {
         this.x = x;
         this.y = y;
-        this.estrategiaMovimiento = estrategia;
-
+        this.estrategiaMovimiento = new MovimientoAleatorio(); 
     }
-    
+
     public void iniciar() {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                estrategiaMovimiento.mover(Enemigo.this);
+                mover(); 
             }
-        }, 0, 10000);
+        }, 0, 1000); 
     }
+    
+    public void mover() {
+        int[] dir = estrategiaMovimiento.calcularDireccion();
+        int nuevaX = this.x + dir[0];
+        int nuevaY = this.y + dir[1];
 
+        Tablero tablero = Tablero.getTablero();
+        if (tablero.esValida(nuevaX, nuevaY)) {
+            Casilla actual = tablero.getCasilla(this.x, this.y);
+            Casilla destino = tablero.getCasilla(nuevaX, nuevaY);
+
+            if (destino.tieneBomberman()) {
+                tablero.getBomberman().morir();
+                return;
+            }
+
+            if (!destino.tieneBloqueDuro() && !destino.tieneBloqueBlando() &&
+                !destino.tieneBomba() && !destino.tieneEnemigo()) {
+                actual.eliminarEnemigo();
+                this.x = nuevaX;
+                this.y = nuevaY;
+                destino.colocarEnemigo(this);
+            }
+        }
+
+        tablero.notificarCambio();
+    }
 
 
     public void cambiarEstrategia(EstrategiaMovimiento nuevaEstrategia) {
@@ -37,10 +63,9 @@ public class Enemigo {
         }
     }
 
-
-    // Getters y setters
     public int getX() { return x; }
     public int getY() { return y; }
     public void setX(int x) { this.x = x; }
     public void setY(int y) { this.y = y; }
 }
+
