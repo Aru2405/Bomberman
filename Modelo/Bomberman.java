de6612e53710f77487307e395f0c16f7a142c823
marioech08 <147819584+marioech08@users.noreply.github.com>
2025-05-03 11:26:50 +0200

    Update Bomberman.java

--- Modelo/Bomberman.java
+++ Modelo/Bomberman.java
@@ -1,47 +1,60 @@
 package Modelo;
 
 public class Bomberman {
-
+	
     private int x;
     private int y;
     private boolean vida;
     private String color;
-    private String ultimaDireccion;
+    String ultimaDireccion;
+    private boolean seHaMovido = false;
 
-    boolean ini = false;
 
+    boolean ini=false;
+   
     public Bomberman(int x, int y, String color) {
         this.x = x;
         this.y = y;
         this.vida = true;
         this.color = color;
-        this.ultimaDireccion = "quieto";
-    }
 
-    public void moverse(int dx, int dy) {
+        
+    }
+    
+    public boolean seHaMovido() {
+        return seHaMovido;
+    }
+    
+    public void moverse(int dx, int dy) {   
         int nuevaX = this.x + dx;
         int nuevaY = this.y + dy;
 
-        // Verificar posicion valida
-        if (!Tablero.getTablero().esValida(nuevaX, nuevaY)) {
-            this.ultimaDireccion = "quieto";
-            Tablero.getTablero().notificarCambio();
-            return;
-        }
+        Tablero tablero = Tablero.getTablero();
+        int filas = tablero.getCeldas().length;
+        int columnas = tablero.getCeldas()[0].length;
 
-        // Verificar obstaculo en medio
+        if (Partida.getPartida().getTipoNivel() == "EMPTY") {
+            if (nuevaX < 0) nuevaX = filas - 1;
+            else if (nuevaX >= filas) nuevaX = 0;
+
+            if (nuevaY < 0) nuevaY = columnas - 1;
+            else if (nuevaY >= columnas) nuevaY = 0;
+        } else {
+            if (!tablero.esValida(nuevaX, nuevaY)) {
+                return;
+            }
+        }
         Casilla casillaDestino = Tablero.getTablero().getCasilla(nuevaX, nuevaY);
         if (casillaDestino.tieneBloqueDuro() || casillaDestino.tieneBloqueBlando() || casillaDestino.tieneBomba()) {
-            this.ultimaDireccion = "quieto";
-            Tablero.getTablero().notificarCambio();
             return;
         }
 
         if (casillaDestino.tieneEnemigo()) {
             morir();
+            return;
         }
 
-        // Guardar la ultima direccion para la animacion
+        // Guardar la última dirección para la animación
         if (dx == -1) {
             ultimaDireccion = "arriba";
         } else if (dx == 1) {
@@ -50,13 +63,10 @@ public class Bomberman {
             ultimaDireccion = "izquierda";
         } else if (dy == 1) {
             ultimaDireccion = "derecha";
-        } else {
-            ultimaDireccion = "quieto";
         }
 
-        Tablero.getTablero().getCasilla(this.x, this.y).eliminarBomberman();
-
         // Mover al Bomberman
+        Tablero.getTablero().getCasilla(this.x, this.y).eliminarBomberman();
         this.x = nuevaX;
         this.y = nuevaY;
 
@@ -65,9 +75,15 @@ public class Bomberman {
             Tablero.getTablero().iniciarEnemigos();
         }
 
+        seHaMovido = true;
         Tablero.getTablero().getCasilla(x, y).colocarBomberman(this);
         Tablero.getTablero().notificarCambio();
+        seHaMovido = false;
     }
+
+
+
+   
 
     public int getX() {
         return x;
@@ -77,11 +93,12 @@ public class Bomberman {
         return y;
     }
 
+
     public void ponerBomba() {
         if (Tablero.getTablero().contarBombasActivas() < 10) {
             Casilla casillaActual = Tablero.getTablero().getCasilla(this.x, this.y);
             System.out.println("Bomba colocada en: (" + x + ", " + y + ")");
-
+    
             Bomba nuevaBomba;
             System.out.println("Color del bomberman: " + this.color);
             System.out.println("Intentando colocar bomba...");
@@ -95,7 +112,7 @@ public class Bomberman {
                 nuevaBomba = new BombaNegra(this.x, this.y);
             } else {
                 System.out.println("⚪ Colocando bomba blanca");
-                nuevaBomba = new Bomba(this.x, this.y);
+                nuevaBomba = new Bomba(this.x, this.y); 
             }
             System.out.println("Tipo real de bomba: " + nuevaBomba.getClass().getSimpleName());
 
@@ -105,36 +122,27 @@ public class Bomberman {
             System.out.println("¡Límite de bombas alcanzado! No puedes colocar más.");
         }
     }
+    
+
 
     public void morir() {
-        vida = false;
+    	vida= false;
         System.out.println("Bomberman ha muerto.");
         Partida.getPartida().terminarJuego();
     }
-
-    public boolean estaVivo() {
-        return vida;
+    
+    public boolean estaVivo(){
+    	return vida;
     }
 
-    public String getUltimaDireccion() {
-        return ultimaDireccion;
-    }
-
-    public void setUltimaDireccion(String dir) {
-        this.ultimaDireccion = dir;
-    }
-
+	public Object getUltimaDireccion() {
+		
+		return  ultimaDireccion;
+	}
+   
     public String getColor() {
         return color;
     }
-    public boolean estaQuieto() {
-    	boolean resul=true;
-    	if (ultimaDireccion.equals("quieto")){
-    		return resul;
-    	}else {
-    		resul=false;
-    		return resul;
-    	}
-    }
+    
 
 }
